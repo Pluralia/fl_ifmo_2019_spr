98ea59f51877b8fcefd72c105aa015467f65462d
Irina Pluralia <irina@metrix.ai>
2019-04-27 03:59:47 +0300

    add tests

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
+
 ./build.sh
-./Main tests/1
-./Main tests/2
-./Main tests/3
-./Main tests/4
-./Main tests/5
-./Main tests/6
-./Main tests/7
-./clean.sh
+
+PATH="tests/*"
+for file in $PATH; do
+    ./Main $file
+done
+
